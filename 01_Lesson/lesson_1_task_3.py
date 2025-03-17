fcb183f4b022c5bd1683680f5bd918f946953416
dionis0101201 <161356912+dionis0101201@users.noreply.github.com>
2025-03-17 20:04:39 +0300

    Add files via upload

---
+++ 01_Lesson/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = ("Руслан")
+last_name = ("Афанасов")
+print("Вас зовут: " + last_name + " " + first_name)
---
+++ 01_Lesson/lesson_1_task_4.py
@@ -0,0 +1,4 @@
+def print_greeting():
+     print("Привет, мир!")
+print("Привет, мир!")
+   
