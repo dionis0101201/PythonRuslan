9c0ccd543574f45aa215995afdb1c8decebfd0c3
dionis0101201 <161356912+dionis0101201@users.noreply.github.com>
2025-03-25 22:10:14 +0300

    Delete lesson_2_task_2.py

--- lesson_2_task_2.py
+++
@@ -1,8 +0,0 @@
-def is_yesr_leap(year):
-    if (year % 4 == 0):
-        print("Year: " + str(year) + " True")
-    else:
-        print("Year: " + str(year) + " False")
-
-
-is_yesr_leap(int(input("Type a year: ")))
